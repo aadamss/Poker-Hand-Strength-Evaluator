66b3059750b54b033995098dfd644289dd4d7809
Arturs Adamss <aadamss@evolution.com>
2021-08-19 15:01:14 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,79 @@
+const CardValues = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
+
+const CombinationValues = {
+  highCard: 100,
+  pair: 200,
+  twoPairs: 300,
+  threeOfAKind: 400,
+  straight: 500,
+  flush: 600,
+  fullHouse: 700,
+  fourOfAKind: 800,
+  straightFlugh: 900,
+};
+
+const [_, _, gameType, baseCards, ...allPlayersCards] = process.argv;
+
+const getValueAndCombinationTexasHoldem = (baseCards, playerCards) => {
+  return {
+    combination: combination,
+    value: value,
+  };
+};
+
+const getValueAndCombinationOmahaHolden = (baseCards, playerCards) => {
+  return {
+    combination: combination,
+    value: value,
+  };
+};
+
+const getValueAndCombinationFiveCardDraw = (baseCards, playerCards) => {
+  return {
+    combination: combination,
+    value: value,
+  };
+};
+
+const result = allPlayersCards
+  .map((onePlayerCards) => {
+    if (gameTyype === 'texas-holdem')
+      return getValueAndCombinationTexasHoldem(baseCards, onePlayerCards);
+    if (gameTyype === 'omaha-holdem')
+      return getValueAndCombinationOmahaHolden(baseCards, onePlayerCards);
+    if (gameTyype === 'five-card-draw')
+      return getValueAndCombinationFiveCardDraw(baseCards, onePlayerCards);
+  })
+  .sort((a, b) => {
+    if (a.value === b.value) {
+      return a.combination > b.combination ? 1 : -1;
+    }
+    return a.value > b.value;
+  })
+  .forEach((result, index, arr) => {
+    if (index === 0) {
+      return console.log(result.combination);
+    }
+
+    if (arr[index - 1].value === result) {
+      console.log('=');
+    } else {
+      console.log(' ');
+    }
+
+    console.log(result.combination);
+  });
